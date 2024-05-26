d6264e2211882b21910f9fbfcc979a26c5bab70a
pfletter <45697044+pfletter@users.noreply.github.com>
2024-05-26 18:54:20 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://designdept-inc.com/",
+  match: "https://designdept-inc.com/**",
+  maxPagesToCrawl: 75,
+  outputFileName: "DDI webCrawl output.json",
 };
